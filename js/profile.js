fb2436a2661aaa6441c390a5910d1475099635f2
Marinto-Richee <marinto3sja@gmail.com>
2023-03-13 00:45:38 +0530

    MongoDB Update

--- js/profile.js
+++ js/profile.js
@@ -1,45 +1,44 @@
-$(document).ready(function () {
+$(document).ready(function() {
 	// get the current user's username from local storage
 	var session_key = localStorage.getItem('session_key');
-
 	// get the user's profile data from MongoDB
 	$.ajax({
 		url: './php/profile.php',
 		type: 'GET',
-		data: { session_key: session_key },
+		data: { session_key: session_key},
 		dataType: 'json',
-		success: function (data) {
-			// populating the username from the database
-			if (data.success) {
+		success: function(data) {
+			// populate the form with the user's profile data
+			if(data.success)
+			{
 				$('#username').val(data.username);
-
-			} else {
+				$('#dob').val(data.dob);
+				$('#age'.val(data.age));
+				$('#contact-address').val(data.contactAddress);
+			}else{
 				localStorage.removeItem('session_key');
 				window.location.href = './login.html';
 			}
+			//alert(data.session_key);
+			//alert(data.message);
 		},
-		error: function () {
+		error: function() {
 			alert('Failed to get user profile data.');
 		}
 	});
 
 	// handle form submission
-	$('#profile-form').submit(function (event) {
-
+	$('#profile-form').submit(function(event) {
 		event.preventDefault();
 		var session_key = localStorage.getItem('session_key');
-
 		// get the form data
-		//var age = $('#age').val();
 		var dob = $('#dob').val();
-
-		//Calculating Age with DOB
 		var date = new Date();
 		var dob_ = new Date(dob);
 		var y2 = date.getFullYear();
 		var y1 = dob_.getFullYear();
 		var age = y2 - y1;
-
+		var username = $('#username').val();
 		var contactAddress = $('#contact-address').val();
 
 		// send the updated profile data to MongoDB
@@ -53,13 +52,13 @@ $(document).ready(function () {
 				dob: dob,
 				contactAddress: contactAddress
 			},
-			success: function () {
+			success: function(data) {
+
 				alert('Profile updated successfully.');
 			},
-			error: function () {
+			error: function() {
 				alert('Failed to update profile.');
 			}
 		});
 	});
 });
-
